5f49439210b3d8811ddfc568ba8fb88ee1d3f65f
BlackDTouch <81647994+BlackDTouch@users.noreply.github.com>
2022-09-04 01:24:56 +0300

    Add files via upload

--- js/counter.js
+++ js/counter.js
@@ -17,9 +17,11 @@ document.addEventListener("DOMContentLoaded", () => {
        }
       }, step);
     }
-    counter("count1", 0, 253, 3000);
-    counter("count2", 100, 252, 2500);
-    counter("count3", 0, 1252, 3000);
-    counter("count4", 0, 52, 3000);
+    counter("count1", 1990, 2015, 3000);
+    counter("count2", 1990, 2017, 3000);
+    counter("count3", 1990, 2018, 3000);
+    counter("count4", 1990, 2019, 3000);
+    counter("count5", 1990, 2020, 3000);
+    counter("count6", 1990, 2021, 3000);
    });
    
